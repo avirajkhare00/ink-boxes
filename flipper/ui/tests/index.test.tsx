855de02716f5b4385c72e57f5ebc2ad9173b573e
Aviraj Khare <avirajkhare00@gmail.com>
2022-11-30 21:36:46 +0530

    [Aviraj] Added test in flipper

--- flipper/ui/tests/index.test.tsx
+++ flipper/ui/tests/index.test.tsx
@@ -1,5 +1,5 @@
 import Home from "../pages/index";
-import { Button } from 'reactstrap';
+import { Button, Input } from 'reactstrap';
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import userEvent from '@testing-library/user-event'
@@ -25,4 +25,20 @@ describe("Flipper", () => {
         await userEvent.click(screen.getByText("Flip"));
         expect(flip).toHaveBeenCalled();
     });
-  });
+    it("should get smart contract output value from input field", async () => {
+        const submitSmartContractAddress = jest.fn();
+        render(
+            <div>
+                <h3>Enter Smart Contract Address</h3>
+                <Input id='smartContractAddress' />
+                <Button onClick={submitSmartContractAddress} color='primary'>Submit</Button>
+                <h3>Smart Contract Address</h3>
+                <Input id="smartContractOutput" disabled></Input>
+            </div>
+        );
+        await userEvent.click(screen.getByText("Submit"));
+        (document.getElementById("smartContractAddress") as HTMLInputElement).value = "abcd123"
+        const smartContractInput = document.getElementById("smartContractAddress");
+        expect((smartContractInput as HTMLInputElement).value).toEqual("abcd123");
+    });
+});
