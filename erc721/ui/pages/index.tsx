c821465929eab02e313fe90b4cc9efcf9bd2052b
Aviraj Khare <avirajkhare00@gmail.com>
2022-12-28 10:55:51 +0530

    [Aviraj] Added subtitles and minor style changes

--- erc721/ui/pages/index.tsx
+++ erc721/ui/pages/index.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { ApiPromise, Keyring, WsProvider } from '@polkadot/api'
 import { Abi, ContractPromise } from '@polkadot/api-contract'
 
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Button, Row, Col } from 'reactstrap';
 
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
@@ -285,102 +285,140 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1>ERC-721 Contract</h1>
-        <Form>
-          <FormGroup>
-            <Label for="address">
-              Enter Smart Contract Address
-            </Label>
-            <Input id="address"></Input>
-            <Button id='submitAddressBtn' color='primary' onClick={submitAddress}>Submit</Button>
-            <br />
-            Smart Contract Address
-            <Input id="smartContractOutput" disabled></Input>
-          </FormGroup>
-          <FormGroup>
-            <Label for="balanceOf">
-              Balance Of
-            </Label>
-            <Input id="balanceOf" placeholder="Enter address"></Input>
-            <Button id='balanceOfBtn' color='primary' onClick={balanceOf}>Submit</Button>
-            <br />
-            <Label for="balanceOfOutput">
-              Output
-            </Label>
-            <Input id="balanceOfOutput" disabled></Input>
-          </FormGroup>
-          <FormGroup>
-            <Label for="ownerOf">
-              Owner Of
-            </Label>
-            <Input id="ownerOf" placeholder="Enter token ID"></Input>
-            <Button id='ownerOfBtn' color='primary' onClick={ownerOf}>Submit</Button>
-            <br />
-            <Label for="ownerOfOutput">
-              Output
-            </Label>
-            <Input id="ownerOfOutput" disabled></Input>
-          </FormGroup>
-          <FormGroup>
-            <Label for="getApproved">
-              Get Approved
-            </Label>
-            <Input id="getApproved" placeholder="Enter token ID" type="number"></Input>
-            <Button id='getApprovedBtn' color='primary' onClick={getApproved}>Submit</Button>
-          </FormGroup>
-          <FormGroup>
-            <Label for="approve">
-              Approve
-            </Label>
-            <Input id="approveAddress" placeholder="Enter approval address"></Input>
-            <Input id="approveTokenId" placeholder="Enter token ID"></Input>
-            <Button id='approveBtn' color='primary' onClick={approve}>Submit</Button>
-            <br />
-            <Label for="approveOutput">
-              Output
-            </Label>
-            <Input id="approveOutput" disabled></Input>
-          </FormGroup>
-          <FormGroup>
-            <Label for="transfer">
-              Transfer
-            </Label>
-            <Input id="transferAddress" placeholder="Enter transfer address"></Input>
-            <Input id="transferTokenId" placeholder="Enter token ID" type='number'></Input>
-            <Button id='transferBtn' color='primary' onClick={transfer}>Submit</Button>
-            <br />
-            <Label for="transferOutput">
-              Output
-            </Label>
-            <Input id="transferOutput" disabled></Input>
-          </FormGroup>
-          <FormGroup>
-            <Label for="transferFrom">
-              Transfer From
-            </Label>
-            <Input id="transferFromFromAddress" placeholder="Enter transfer from address"></Input>
-            <Input id="transferFromToAddress" placeholder="Enter transfer to address"></Input>
-            <Input id="transferFromTokenId" placeholder="Enter token ID" type='number'></Input>
-            <Button id='transferFromBtn' color='primary' onClick={transferFrom}>Submit</Button>
-            <br />
-            <Label for="transferFromOutput">
-              Output
-            </Label>
-            <Input id="transferFromOutput" disabled></Input>
-          </FormGroup>
-          <FormGroup>
-            <Label for="mint">
-              Mint
-            </Label>
-            <Input id="mintTokenId" placeholder="Enter token ID"></Input>
-            <Button id='mintTokenBtn' color='primary' onClick={mint}>Submit</Button>
-            <br />
-            <Label for="mintOutput">
-              Output
-            </Label>
-            <Input id="mintOutput" disabled></Input>
-          </FormGroup>
-        </Form>
+        <Row>
+          <Col md='2'></Col>
+          <Col md='8'>
+            <h1>ERC-721 Contract</h1>
+            <Form>
+              <FormGroup>
+                <Label for="address">
+                  Enter Smart Contract Address
+                  <br />
+                  <span style={{"fontSize": "12px"}}>
+                    Enter your erc20 smart contract address to use the UI.
+                  </span>
+                </Label>
+                <Input id="address"></Input>
+                <Button id='submitAddressBtn' color='primary' onClick={submitAddress}>Submit</Button>
+                <br />
+                Smart Contract Address
+                <Input id="smartContractOutput" disabled></Input>
+              </FormGroup>
+              <FormGroup>
+                <Label for="balanceOf">
+                  Balance Of
+                  <br />
+                  <span style={{"fontSize": "12px"}}>
+                    Gives balance of a given address.
+                  </span>
+                </Label>
+                <Input id="balanceOf" placeholder="Enter address"></Input>
+                <Button id='balanceOfBtn' color='primary' onClick={balanceOf}>Submit</Button>
+                <br />
+                <Label for="balanceOfOutput">
+                  Output
+                </Label>
+                <Input id="balanceOfOutput" disabled></Input>
+              </FormGroup>
+              <FormGroup>
+                <Label for="ownerOf">
+                  Owner Of
+                  <br />
+                  <span style={{"fontSize": "12px"}}>
+                    Gives who is owner of given NFT ID.
+                  </span>
+                </Label>
+                <Input id="ownerOf" placeholder="Enter token ID"></Input>
+                <Button id='ownerOfBtn' color='primary' onClick={ownerOf}>Submit</Button>
+                <br />
+                <Label for="ownerOfOutput">
+                  Output
+                </Label>
+                <Input id="ownerOfOutput" disabled></Input>
+              </FormGroup>
+              <FormGroup>
+                <Label for="getApproved">
+                  Get Approved
+                  <br />
+                  <span style={{"fontSize": "12px"}}>
+                    Get approved to send token on behalf of an owner.
+                  </span>
+                </Label>
+                <Input id="getApproved" placeholder="Enter token ID" type="number"></Input>
+                <Button id='getApprovedBtn' color='primary' onClick={getApproved}>Submit</Button>
+              </FormGroup>
+              <FormGroup>
+                <Label for="approve">
+                  Approve
+                  <br />
+                  <span style={{"fontSize": "12px"}}>
+                    Approve to address with token id so that spender can take action on your behalf.
+                  </span>
+                </Label>
+                <Input id="approveAddress" placeholder="Enter approval address"></Input>
+                <Input id="approveTokenId" placeholder="Enter token ID"></Input>
+                <Button id='approveBtn' color='primary' onClick={approve}>Submit</Button>
+                <br />
+                <Label for="approveOutput">
+                  Output
+                </Label>
+                <Input id="approveOutput" disabled></Input>
+              </FormGroup>
+              <FormGroup>
+                <Label for="transfer">
+                  Transfer
+                  <br />
+                  <span style={{"fontSize": "12px"}}>
+                    Transfer a token to different address.
+                  </span>
+                </Label>
+                <Input id="transferAddress" placeholder="Enter transfer address"></Input>
+                <Input id="transferTokenId" placeholder="Enter token ID" type='number'></Input>
+                <Button id='transferBtn' color='primary' onClick={transfer}>Submit</Button>
+                <br />
+                <Label for="transferOutput">
+                  Output
+                </Label>
+                <Input id="transferOutput" disabled></Input>
+              </FormGroup>
+              <FormGroup>
+                <Label for="transferFrom">
+                  Transfer From
+                  <br />
+                  <span style={{"fontSize": "12px"}}>
+                    Owner can transfer a token from a address to a address.
+                  </span>
+                </Label>
+                <Input id="transferFromFromAddress" placeholder="Enter transfer from address"></Input>
+                <Input id="transferFromToAddress" placeholder="Enter transfer to address"></Input>
+                <Input id="transferFromTokenId" placeholder="Enter token ID" type='number'></Input>
+                <Button id='transferFromBtn' color='primary' onClick={transferFrom}>Submit</Button>
+                <br />
+                <Label for="transferFromOutput">
+                  Output
+                </Label>
+                <Input id="transferFromOutput" disabled></Input>
+              </FormGroup>
+              <FormGroup>
+                <Label for="mint">
+                  Mint
+                  <br />
+                  <span style={{"fontSize": "12px"}}>
+                    Mints a new Token.
+                  </span>
+                </Label>
+                <Input id="mintTokenId" placeholder="Enter token ID"></Input>
+                <Button id='mintTokenBtn' color='primary' onClick={mint}>Submit</Button>
+                <br />
+                <Label for="mintOutput">
+                  Output
+                </Label>
+                <Input id="mintOutput" disabled></Input>
+              </FormGroup>
+            </Form>
+          </Col>
+          <Col md='2'></Col>
+        </Row>
       </main>
     </div>
   )
